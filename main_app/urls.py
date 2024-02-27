b534082a54c3f7dd64374c2f88dbf6c7ade837fb
Mathew <muckele7@gmail.com>
2024-02-27 00:21:55 -0500

    completes part 1 of dog collector lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('dogs/', views.dog_index, name='dog-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# Add the Cat class & list and view function below the imports
+class Dog:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
+  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def dog_index(request):
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
