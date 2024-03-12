14d9062eb7446e60ea28b87a8638737b57bccead
Mathew <muckele7@gmail.com>
2024-03-12 10:02:44 -0400

    updates nav bar styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
   path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dog-update'),
   path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dog-delete'),
   path('dogs/<int:dog_id>/add-feeding/', views.add_feeding, name='add-feeding'),
-  path('dogs/<int:dog_id>/assoc-toy/<int:toy_id>/', views.assoc_toy, name='assoc-toy'),
-  path('accounts/signup/', views.signup, name='signup'),
   path('toys/create/', views.ToyCreate.as_view(), name='toy-create'),
   path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
   path('toys/', views.ToyList.as_view(), name='toy-index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete'),
+  path('dogs/<int:dog_id>/assoc-toy/<int:toy_id>/', views.assoc_toy, name='assoc-toy'),
+  path('accounts/signup/', views.signup, name='signup'),
   
 ]
